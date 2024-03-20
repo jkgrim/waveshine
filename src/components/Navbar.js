4fbdb20205cf8887dcba72803c540b4bdcdf4a58
jkgrim <j.grimshaw37@gmail.com>
2024-03-19 22:16:28 -0600

    adds navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="waveshine-logo">
+        <div className="navlink-wrapper"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
